6373eb2213bd56cf6edda4e875df91ad6efda7f8
GadhaSasiA <144037281+GadhaSasiA@users.noreply.github.com>
2024-08-31 23:10:05 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,41 @@
+# *printing
+row=5
+for i in range(0,row+1):
+   for j in range(1,i+1):
+      print("*",end='')
+   print("")
+
+
+# number pattern
+row=5
+for i in range(1,row+1):
+   for j in range(1,i+1):
+      print(j,end='')
+   print("")
+
+
+row=5
+for i in range(1,row+1):
+   for j in range(1,i+1):
+      print(i,end='')
+   print("")
+
+row=5
+for i in range(1,row+1):
+   for j in range(1,i+1):
+      print(j,end='')
+   print("")
+
+
+row=5
+i=1
+while i<=row:
+    j=1
+    while j<=5:
+        print("*",end='')
+        j=j+1
+    print("")
+    i=i+1
+
+
+
